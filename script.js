69ce2e67f1cebaf5068c68a0b3716421f5f910fb
Steven <stevenpjohnson@pm.me>
2023-04-25 15:57:29 -0500

    2023.1

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function toggleDarkMode() {
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-mode');
-  }  
+// Add a click event listener to the navigation menu items
+var navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    var sectionId = this.getAttribute('href');
+    var section = document.querySelector(sectionId);
+    section.scrollIntoView({behavior: 'smooth'});
+  });
+});
